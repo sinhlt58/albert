c2ecf268d279540261ec6e777c884ae8ef6bf29d
sinhlt58 <sinhlt58@gmail.com>
2019-11-25 20:22:20 +0700

    update

--- run_classifier_sp.py
+++ run_classifier_sp.py
@@ -1066,7 +1066,8 @@ def main(_):
         drop_remainder=predict_drop_remainder)
 
     result = estimator.predict(input_fn=predict_input_fn)
-    print ("result: ", result)
+    tf.logging.info("probabilities: ", result["probabilities"])
+    tf.logging.info("predictions: ", result["predictions"])
 
     output_predict_file = os.path.join(FLAGS.output_dir, "test_results.tsv")
     output_submit_file = os.path.join(FLAGS.output_dir, "submit_results.tsv")
